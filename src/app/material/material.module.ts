3f75abc48532acbd07cb82e7c1b81d0f24c6e6e7
ruhollahm8 <graphist77@gmail.com>
2018-06-02 07:54:53 +0430

    Design Header Component & Add images & SetTitle index

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import {
   MatToolbarModule,
   MatButtonModule,
+  MatCardModule,
 } from '@angular/material';
 
 @NgModule({
   imports: [
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
   ]
 })
 export class MaterialModule { }
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private titleService: Title
+  ) { }
 
   ngOnInit() {
+    this.titleService.setTitle('وبلاگ شخصی من');
   }
 
 }
